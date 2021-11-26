a331f64ec859264aff912aba308521351a633cd7
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-27 02:23:21 +0300

    updated api code

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
-package main 
+package main
 
 import (
+	"github.com/KenMwaura1/go-todo-api/database"
+	"github.com/KenMwaura1/go-todo-api/todo"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 import "fmt"
 
@@ -12,11 +14,14 @@ func main() {
 	fmt.Println("App running!")
 	app := fiber.New()
 	app.Use(cors.New())
+	database.ConnectDB()
+	defer database.DB.Close()
 
 	api := app.Group("/api")
 	api.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+	todo.Register(api, database.DB)
 
 	log.Fatal(app.Listen(":5002"))
 }
