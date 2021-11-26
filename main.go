2da4472c02dfd4c1b81dcc6fa94cb980db87f6de
Kennedy-Mwaura <kemwaura@gmail.com>
2021-11-26 14:37:36 +0300

    created an endpoint

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main 
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
+)
 import "fmt"
 
 func main() {
 	fmt.Println("App running!")
+	app := fiber.New()
+	app.Use(cors.New())
+
+	api := app.Group("/api")
+	api.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":5002"))
 }
